5fc93bc11b4b466215ad2d5cd0e4966c9637d926
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2023-05-16 23:50:47 +0000

    some tweaking around

--- kaboom/app/page.tsx
+++ kaboom/app/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <main>
-      <h1>kaboom</h1>
+    <main className={styles.main}>
+      <header className={styles.header}>
+        <h2>Search with the Power of AI Technology</h2>
+        <h1>The Explosive Way to Find Anything Online</h1>
+        <Link className={styles.cta} href="/products">
+          start building for free
+        </Link>
+      </header>
+      <section className={styles.section}>
+        <h1>Why leaders are choosing Kaboom...</h1>
+      </section>
     </main>
   );
 }
